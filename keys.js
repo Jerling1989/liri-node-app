4fb535218396b7d651355c40f8d4c30614a9b550
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-06-20 10:04:53 -0400

    twitter keys

---
+++ keys.js
@@ -0,0 +1,9 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'glTzteVmdhvhCAlCc5ShEjq4K',
+  consumer_secret: '7f0kE8hctFBIjxY0JLwlv5BL7zb53gfuSlxAlAaA36fz8Q2Qqh',
+  access_token_key: '877161277902270464-cj6MwD1IXGs8HbbVfqCZ0uP2xPWa2rr',
+  access_token_secret: 'gvKtdq8ZdQFGrcRiJuwsQ4pEQWdIhqEysYYSRFoun8q0a',
+}
